e0df78adaa5e9d0f38539619481a3a1078a61d75
Pablo Vasquez <pablo_vasquez@Pablos-MacBook-Pro.local>
2021-04-28 15:24:58 -0400

    ORDER UPgit add .

---
+++ gimmemynumber.py
@@ -0,0 +1,12 @@
+userstring = input("Gimme a number that is greater than 100...")
+usernum = int(userstring);
+
+while (usernum < 100):
+# test condition; for when the termination is NOT true, LOOP AWAY!!
+ # "while usernum is less than 100..."
+ print(usernum + " is less than 100, Try again,  I've got all day...")
+
+ userstring = print(usernum + "is less than 100, Try again,  I've got all day...")
+
+ usernum = parseInt(userstring)
+ print(usernum + " is greater than 100! Finally! Congratulations!!")
